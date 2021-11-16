4a62f5feb573382ee5a21df42aa1e25ac493c3e3
aroranipun12 <aroranipun1@gmail.com>
2021-11-16 13:59:14 +0530

    added routing

--- index.js
+++ index.js
@@ -4,19 +4,59 @@ const url = require("url");
 
 //SERVER
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-const productData = JSON.parse(data);
+const replacePlaceholder = (template, product) => {
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/%FROM%/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduce = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+//array of object
+const dataObj = JSON.parse(data);
 const server = http.createServer((req, res) => {
   const pathName = req.url;
+
+  //Overview page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is OVERVIEW");
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const finalHtml = dataObj
+      .map((el) => replacePlaceholder(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", finalHtml);
+    res.end(output);
+
+    //Product page
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
+
+    //API
   } else if (pathName === "/api") {
     //we need to tell browser we are sending json(for formatting)
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
+
+    //Not Found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
